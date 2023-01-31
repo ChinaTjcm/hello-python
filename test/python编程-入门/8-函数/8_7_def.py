65f02c456afb70c24e1c362d30792a1343b412f5
陈明 <644352469@qq.com>
2023-01-31 17:33:52 +0800

    :art: 《Python编程：从入门到实践（第2版） ([美]埃里克·马瑟斯（Eric Matthes）) (z-lib.org)》 学习 8 章 7 节 传值比较重要

---
+++ test/python编程-入门/8-函数/8_7_def.py
@@ -0,0 +1,7 @@
+from user_profile_8_6 import build_profile
+
+# 字典
+user_profile = build_profile('albert', 'einstein',
+                             location='princeton',
+                             field='physics')
+print(user_profile)
