8d1bbe8b7646f5be11582e79a777febbf261b5d7
chenming <chenming@szhtkj.com.cn>
2023-02-01 23:04:46 +0800

    解决编译错误

--- test/python编程-入门/9-类/Car_9_2.py
+++ test/python编程-入门/9-类/Car_9_2.py
@@ -3,7 +3,7 @@ class Car:
     一辆汽车
     """
 
-    def __int__(self, make, model, year):
+    def __init__(self, make, model, year):
         self.make = make
         self.model = model
         self.year = year
@@ -23,6 +23,6 @@ class Car:
 
 
 my_car = Car('奔驰', 'S500', 2023)
-my_car.update_odometer(2300)
-my_car.increment_odometer(1000)
+my_car.update_odometer(23_500)
+my_car.increment_odometer(100)
 print(f"我的车最新公里数，应该是 3300 ，现在输出为：{my_car.odometer_reading}")
