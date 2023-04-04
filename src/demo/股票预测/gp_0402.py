dcfbe2c5264f91011733898c377917298c46177f
陈明 <644352469@qq.com>
2023-04-05 01:04:56 +0800

    :fire: 股票demo提交

--- src/demo/股票预测/gp_0402.py
+++ src/demo/股票预测/gp_0402.py
@@ -1,7 +1,19 @@
+from jqdatasdk import get_factor_values
+import jqdatasdk
+
+jqdatasdk.auth('13652089358', 'Charminglife0313')
+
+
+# 程序运行 demo
+# https://www.joinquant.com/research?target=research&url=/user/59556498210/notebooks/Untitled.ipynb?kernel_name=python3
+
 # 导入函数库
 from jqfactor import get_factor_values
-# 载入函数库
-import pandasaspd
-import jqfactor_analyzerasja
-import jqdatasdkjqdatasdk.auth('13652089358','Charminglife0313')
-# 获取5日平均换手率因子2018-01-01到2018-12-31之间的数据（示例用从库中直接调取）# 聚宽因子库数据获取方法在下方
+# 取值函数
+factor_data = get_factor_values(securities=['000001.XSHE'], factors=['BR'], start_date='2012-04-01', end_date='2022-02-01')
+print(factor_data['BR'])
+# 获取因子Skewness60(个股收益的60日偏度)从 2017-01-01 至 2017-03-04 的因子值
+# factor_data = get_factor_values(securities=['000001.XSHE'], factors=['BR'], start_date='2017-01-01', end_date='2017-03-04')
+# 查看因子值
+# print(factor_data)
+# factor_data['Skewness60']
--- test/股票预测/gp_0402.py
+++
@@ -1,7 +0,0 @@
-# 导入函数库
-from jqfactor import get_factor_values
-# 载入函数库
-import pandasaspd
-import jqfactor_analyzerasja
-import jqdatasdkjqdatasdk.auth('13652089358','Charminglife0313')
-# 获取5日平均换手率因子2018-01-01到2018-12-31之间的数据（示例用从库中直接调取）# 聚宽因子库数据获取方法在下方
